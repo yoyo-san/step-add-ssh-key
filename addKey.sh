d7fd4a0dc01661c9e6dfaa70d2d80aea57975e55
Jacco Flenter <github@putpixel.nl>
2014-11-06 13:59:49 +0100

    fixed: host parameter

--- addKey.sh
+++ addKey.sh
@@ -9,7 +9,7 @@ if [ ! -f $1/.ssh/config ]; then
   if [ -z $4 ]; then
      echo -e "Host *\n    IdentityFile $3" > $1/.ssh/config
   else
-     echo -e "Host $host\n    IdentityFile $3" > $1/.ssh/config
+     echo -e "Host $4\n    IdentityFile $3" > $1/.ssh/config
   fi
   chown $2 $1/.ssh/config
   chmod 0600 $1/.ssh/config
@@ -20,7 +20,7 @@ else
   if [ -z $4 ]; then
     sed -i -e "1i Host *" $1/.ssh/config
   else
-    sed -i -e "1i Host $host" $1/.ssh/config
+    sed -i -e "1i Host $4" $1/.ssh/config
   fi
   echo "$1/.ssh/config updated"
 fi
