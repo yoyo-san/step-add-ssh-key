f9b047faa3d215be52189cf3a9d4f04b07563135
yoyo-san <rokmirt@gmail.com>
2015-03-23 11:47:53 +0100

    Update addKey.sh

--- addKey.sh
+++ addKey.sh
@@ -9,7 +9,7 @@ if [ ! -f $1/.ssh/config ]; then
   if [ -z $5 ]; then
      echo -e "Host *\n    IdentityFile $3\n    Port $4" > $1/.ssh/config
   else
-     echo -e "Host $5\n    IdentityFile $3    Port $4" > $1/.ssh/config
+     echo -e "Host $5\n    IdentityFile $3\n    Port $4" > $1/.ssh/config
   fi
   chown $2 $1/.ssh/config
   chmod 0600 $1/.ssh/config
